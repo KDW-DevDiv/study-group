0686b8ad6e0e866da7baffa38429f895a841ae95
Ippei Horiba <ip.horiba@gmail.com>
2022-08-05 09:22:22 +0900

    2つのサンプルの差分を修正。文言も一部修正

--- 1.React/result/with-react/src/app.js
+++ 1.React/result/with-react/src/app.js
@@ -1,28 +1,50 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
 import './styles/app.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-  }
+// Function Component
+const App = () => {
+  const [value, setValue] = useState('');
 
-  render() {
-    return (
-      <div>
-        <button
-          className="button"
-          onClick={() => {
-            this.setState({ value: 'Hello World' });
-          }}
-        >
-          Click!
-        </button>
-        <br />
-        <input type="textare" readOnly value={this.state.value} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <button
+        className="button"
+        onClick={() => {
+          setValue('Hello World');
+        }}
+      >
+        Click!
+      </button>
+      <br />
+      <textarea readOnly value={value} />
+    </div>
+  );
+};
+
+// Class Component
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { value: '' };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button
+//           className="button"
+//           onClick={() => {
+//             this.setState({ value: 'Hello World' });
+//           }}
+//         >
+//           Click!
+//         </button>
+//         <br />
+//         <input type="textare" readOnly value={this.state.value} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- 1.React/result/without-react/server.js
+++ 1.React/result/without-react/server.js
@@ -15,5 +15,5 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000);
-console.log('Node server running on port 3000');
+server.listen(3001);
+console.log('Node server running on port 3001');
