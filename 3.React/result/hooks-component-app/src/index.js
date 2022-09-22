5e6c18102ee149a8d036ee0abb510f30ba8f9f10
Ippei Horiba <ip.horiba@gmail.com>
2022-09-22 12:20:55 +0900

    Example Codeのコメントアウトを解除

--- 3.React/result/hooks-component-app/src/index.js
+++ 3.React/result/hooks-component-app/src/index.js
@@ -2,19 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import EffectComponent from './components/effect-component';
+import EffectPComponent from './components/effect-p-component';
 import HelloComponent from './components/hello-component';
 import MountUnMountComponent from './components/mount-unmount-component';
 import ParentComponent from './components/parent-component';
-import EffectPComponent from './components/effect-p-component';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// console.log(<HelloComponent />);
+console.log(<HelloComponent />);
 root.render(
   <div>
-    {/* <HelloComponent /> */}
-    {/* <MountUnMountComponent /> */}
-    {/* <ParentComponent /> */}
+    <HelloComponent />
+    <MountUnMountComponent />
+    <ParentComponent />
     <EffectPComponent />
   </div>
 );
